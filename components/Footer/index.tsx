a8f67032922b6a1be1de628d10b832a27a943957
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-04-18 19:32:59 -0300

    feat(components): Add Footer

---
+++ components/Footer/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+const Footer: React.FunctionComponent<any> = () => (
+  <footer>
+    <div>
+    </div>
+    <style jsx>{`
+      footer {
+        color: #abcdef;
+        display: block;
+        position: relative;
+        font-family: 'Roboto Slab',serif;
+      }
+    `}</style>
+  </footer>
+
+)
+
+export default Footer
